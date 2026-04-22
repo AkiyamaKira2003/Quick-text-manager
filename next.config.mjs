24eedfa83b99edfb7641f6de5beac87816ef518c
Kira <phamthanhtuan1711@gmail.com>
2026-04-22 14:49:17 +0700

    fix: restore next config for v2.0.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,15 @@
-asdadasdsa
+import createBundleAnalyzer from '@next/bundle-analyzer'
+
+const withBundleAnalyzer = createBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: { unoptimized: true },
+  devIndicators: false,
+  distDir: process.env.QT_NEXT_DIST_DIR || '.next',
+}
+
+export default withBundleAnalyzer(nextConfig)
